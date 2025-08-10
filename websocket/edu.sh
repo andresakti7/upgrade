6590487abac0ce1382290d350b387225291189fb
andresakti7 <117853441+andresakti7@users.noreply.github.com>
2025-08-11 00:15:18 +0700

    Add files via upload

---
+++ websocket/edu.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Proxy For Edukasi & Imclass
+# My Telegram : https://t.me/AnuyBazoelk
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+clear
+red='\e[1;31m'
+green='\e[0;32m'
+yell='\e[1;33m'
+NC='\e[0m'
+echo "Installing SSH WSS by Dragon Emperor" | lolcat
+echo "Progress..." | lolcat
+sleep 3
+cd
+# Link Hosting Kalian
+bzvpn="raw.githubusercontent.com/Andyyuda/project/main/websocket"
+
+# Getting Proxy Template
+wget -q -O /usr/local/bin/ws-tls https://${bzvpn}/ws-tls
+chmod +x /usr/local/bin/ws-tls
+
+# Installing Service
+cat > /etc/systemd/system/ws-tls.service << END
+[Unit]
+Description=Python Proxy Mod By AnuyBazoelk
+Documentation=https://t.me/AnuyBazoelk
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-tls 443
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-tls
+systemctl restart ws-tls
+
+rm -f edu.sh
